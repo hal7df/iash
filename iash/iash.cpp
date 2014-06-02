44f5035f3a3141cab12754a55de2f46c8a726bf5
hal7df <hal7df@gmail.com>
2014-06-02 10:43:45 -0400

    Added debug console and environment system.
    Added app name and basic configuration options.

--- iash/iash.cpp
+++ iash/iash.cpp
@@ -1,5 +1,16 @@
 #include "iash.h"
 
+iash::iash (string app_nm, bool useInPrompt)
+{
+    cout<<"iash version 0.1 initializing..."<<endl;
+
+    m_appName = app_nm;
+    f_useAppNameInPrompt = useInPrompt;
+
+    setenv("APP_NAME",m_appName);
+    setenv("APP_NAME_IN_PROMPT",f_useAppNameInPrompt);
+}
+
 vector<string> iash::getCmdLine()
 {
     string raw;
@@ -7,6 +18,12 @@ vector<string> iash::getCmdLine()
     vector<string> cmdLine;
 
     clear();
+    updateAttached();
+
+    if (f_useAppNameInPrompt)
+        cout<<m_appName<<"> ";
+    else
+        cout<<">>> ";
 
     getline(cin,raw);
     lastSpace = 0;
@@ -27,7 +44,274 @@ vector<string> iash::getCmdLine()
 
     }while(space != string::npos);
 
+    if (cmdLine[0] == "iash")
+    {
+        if (cmdLine.size() > 1)
+            debugConsole(cmdLine);
+        else
+            debugConsole();
+    }
     m_cmdLine = cmdLine;
 
     return cmdLine;
+}
+
+vector<string> iash::getOptions(vector<string> *cmd)
+{
+    int x,y;
+    vector<string> options;
+
+    for (x=0; x < cmd->size(); x++)
+    {
+        if (cmd->at(x).find('-') != string::npos)
+        {
+            if (cmd->at(x).find("--") != string::npos)
+            {
+                options.push_back(cmd->at(x).substr(2));
+            }
+            else if (cmd->at(x).length() < 2)
+            {
+                for (y = 0; y < cmd->at(x).length(); y++)
+                    options.push_back(cmd->at(x)[y]);
+            }
+            else
+                options.push_back(cmd->at(x).substr(1));
+        }
+    }
+
+    return options;
+}
+
+void iash::cmdNotFound()
+{
+    cout<<m_cmdLine[0]<<": command not found"<<endl;
+}
+
+void iash::cmdNotFound_dbg()
+{
+    cout<<m_cmdLine[1]<<": command not found"<<endl;
+}
+
+void iash::clearScreen()
+{
+    #ifdef __unix__
+        if (!cur_term)
+        {
+            int result;
+            setupterm(NULL, STDOUT_FILENO, &result);
+            if (result <= 0)
+            return;
+        }
+
+        putp(tigetstr("clear"));
+    #elif _WIN32
+        HANDLE                     hStdOut;
+          CONSOLE_SCREEN_BUFFER_INFO csbi;
+          DWORD                      count;
+          DWORD                      cellCount;
+          COORD                      homeCoords = { 0, 0 };
+
+          hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+          if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+          /* Get the number of cells in the current buffer */
+          if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+          cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+          /* Fill the entire buffer with spaces */
+          if (!FillConsoleOutputCharacter(
+            hStdOut,
+            (TCHAR) ' ',
+            cellCount,
+            homeCoords,
+            &count
+            )) return;
+
+          /* Fill the entire buffer with the current colors and attributes */
+          if (!FillConsoleOutputAttribute(
+            hStdOut,
+            csbi.wAttributes,
+            cellCount,
+            homeCoords,
+            &count
+            )) return;
+
+          /* Move the cursor home */
+          SetConsoleCursorPosition( hStdOut, homeCoords );
+    #endif
+}
+
+void iash::setenv(string name, string value)
+{
+    transform(name.begin(),name.end(),name.begin(),toupper);
+
+    if (m_env.find(name) == m_env.end())
+        m_env.emplace(name,value);
+    else
+        m_env.at(name) = value;
+}
+
+void iash::setenv(string name, bool value)
+{
+    if (value)
+        setenv(name,"true");
+    else
+        setenv(name,"false");
+}
+
+bool iash::getenv_bool(string name)
+{
+    if (m_env.at(name) == "true")
+        return true;
+    else
+        return false;
+}
+
+void iash::rmenv(string name)
+{
+    if (name != "APP_NAME" && name != "APP_NAME_IN_PROMPT")
+        m_env.erase(name);
+}
+
+void iash::setAppName(string app_nm)
+{
+    m_appName = app_nm;
+    setenv("APP_NAME",app_nm);
+}
+
+void iash::useAppNameInPrompt(bool name)
+{
+    f_useAppNameInPrompt = name;
+    setenv("APP_NAME_IN_PROMPT",name);
+}
+
+void iash::debugConsole()
+{
+    vector<string> cmdLine;
+
+    m_appName = "iash";
+    f_useAppNameInPrompt = true;
+
+    do {
+        cmdLine = getCmdLine();
+        cmdLine.insert(cmdLine.begin(),"iash");
+
+        debugConsole(cmdLine);
+    } while (cmdLine[1] != "exit");
+
+    m_appName = getenv_string("APP_NAME");
+    f_useAppNameInPrompt = getenv_string("APP_NAME_IN_PROMPT");
+    clear();
+}
+
+void iash::debugConsole(vector<string> cmd)
+{
+    int x;
+
+    if (cmd[1] == "cnf")
+    {
+        cout<<"DBG: IASH_CNF_TEST"<<endl;
+        cmdNotFound();
+    }
+    else if (cmd[1] == "clear")
+    {
+        if (cmd.size() > 2)
+        {
+            if (cmd[2] == "screen")
+                clearScreen();
+            else if (cmd[2] == "buf" || cmd[2] == "buffer")
+                clear();
+            else
+                cout<<cmd[2]<<": not a valid clear argument"<<endl;
+        }
+        else
+            clear();
+    }
+    else if (cmd[1] == "disp")
+    {
+        if (cmd.size() > 2)
+        {
+            if (cmd[2] == "cmd")
+            {
+                for (x = 0; x < cmd.length(); x++)
+                    cout<<cmd.at(x)<<endl;
+            }
+            else if (cmd[2] == "ops")
+            {
+                vector<string> ops;
+                ops = getOps(&cmd);
+                for (x = 0; x < ops.size(); x++)
+                    cout<<ops.at(x)<<endl;
+            }
+            else if (cmd[2] == "env")
+            {
+                map<string,string>::iterator it;
+
+                for (it = m_env.begin(); it != m_env.end(); ++it)
+                    cout<<it->first<<' '<<it->second<<endl;
+            }
+            else
+                cout<<cmd[2]<<": invalid "<<cmd[1]<<" argument"<<endl;
+        }
+        else
+            cout<<cmd[1]<<": need an argument"<<endl;
+    }
+    else if (cmd[1] == "set")
+    {
+        if (cmd.size() > 2)
+        {
+            if (cmd[2] == "env")
+            {
+                if (cmd.size() == 5)
+                {
+                    if (cmd[3] == "-r")
+                        rmenv(cmd[4]);
+                    else
+                        setenv(cmd[3],cmd[4]);
+                }
+                else
+                    cout<<"Error: improper usage"<<endl<<"Usage: iash set env (-r) [NAME] [VALUE]"<<endl;
+            }
+            else if (cmd[2] == "appInPrompt")
+            {
+                if (cmd.size() > 3)
+                {
+                    if (cmd[4] == "on")
+                        useAppNameInPrompt();
+                    else if (cmd[4] == "off")
+                        useAppNameInPrompt(false);
+                    else
+                        cout<<"Error: improper usage"<<endl<<"Usage: iash set appInPrompt <on/off> (optional)"<<endl;
+                }
+                else
+                    useAppNameInPrompt();
+            }
+            cout<<cmd[1]<<": invalid"<<cmd[1]<<" argument"<<endl;
+        }
+        else
+            cout<<cmd[1]<<": need an argument"<<endl;
+    }
+    else if (cmd[1] == "app")
+    {
+        if (cmd.size() > 2)
+        {
+            if (cmd[2] == "quit")
+                exit(0);
+            else if (cmd[2] == "kill" || cmd[2] == "crash")
+                exit(1);
+        }
+        else
+            cout<<getenv_string("APP_NAME")<<endl;
+    }
+    else if (cmd[1] != "exit")
+        cmdNotFound_dbg();
+}
+
+void iash::updateAttached()
+{
+    if (getenv_string("APP_NAME") != m_appName)
+        m_appName = getenv_string("APP_NAME");
+
+    if (getenv_string("APP_NAME_IN_PROMPT") != f_useAppNameInPrompt)
+        f_useAppNameInPrompt = getenv_bool("APP_NAME_IN_PROMPT");
 }
--- iash/iash.h
+++ iash/iash.h
@@ -12,21 +12,42 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
+#include <algorithm>
 #include <string>
 #include <limits>
+#include <cstdlib>
+#include <cctype>
+
+#ifdef __unix__
+    #include <unistd.h>
+    #include <term.h>
+#elif _WIN32
+    #include <windows.h>
+#endif
 
 using namespace std;
-
-typedef struct {
-    string option;
-    string argument;
-} option_t;
 
 class iash
 {
 public:
-    iash() {}
+    iash(string app_nm="iash", bool useNameInPrompt=true);
     ~iash() {}
+
+    /**
+     * @brief setAppName: Set the application name. This can be used
+     *                    on the prompt.
+     * @param app_nm: The name of the application.
+     */
+    void setAppName (string app_nm) { m_appName = app_nm; }
+
+    /**
+     * @brief useAppNameInPrompt: Show the app name in the command
+     *                            prompt.
+     * @param name: Whether or not to use the app name in the command
+     *              prompt.
+     */
+    void useAppNameInPrompt (bool name=true);
 
     /**
      * @brief getCmdLine: Clear the current shell contents and prompt a
@@ -44,9 +65,35 @@ public:
 
     /**
      * @brief getOptions: Get the options from the last getCmdLine() call
-     * @return: all of the options and the arguments from those options
+     * @return: all of the options passed at the commandline
      */
-    vector<option_t> getOptions();
+    vector<string> getOptions(vector<string> cmd=m_cmdLine);
+
+    /**
+     * @brief setenv: Set internal environment variables
+     * @param name: The name of the variable
+     * @param value: The value of the variable
+     */
+    void setenv (string name, string value);
+    void setenv (string name, int value) { setenv(name,to_string(value)); }
+    void setenv (string name, float value) { setenv(name,to_string(value)); }
+    void setenv (string name, bool value);
+
+    /**
+     * @brief getenv: Get internal environment variables
+     * @param name: The name of the variable
+     * @return: The variable
+     */
+    string getenv_string (string name) { return m_env.at(name); }
+    int getenv_int (string name) { return stoi(m_env.at(name)); }
+    float getenv_float (string name) { return stof(m_env.at(name)); }
+    bool getenv_bool (string name);
+
+    /**
+     * @brief rmenv: Remove internal environment variables
+     * @param name: The name of the variable to remove
+     */
+    void rmenv (string name);
 
     /**
      * @brief cmdNotFound: Prints a command not found message and clears the
@@ -57,11 +104,22 @@ public:
     /**
      * @brief clear: Clear the command buffer.
      */
-    void clear();
+    void clear() { m_cmdLine.clear(); }
+
+    /**
+     * @brief clearScreen: Clear the screen.
+     */
+    void clearScreen();
 private:
     void debugConsole (vector<string> cmd);
+    void debugConsole ();
+    void cmdNotFound_dbg();
+    void updateAttached();
 
     vector<string> m_cmdLine;
+    map<string,string> m_env;
+    string m_appName;
+    bool f_useAppNameInPrompt;
 };
 
 #endif //IASH_H
