cc171564ac480b77930283e1bb7b84e270d2401d
Chad A <Chad A>
2016-11-26 16:07:25 -0500

    Added EchoCommand as first builtin command

---
+++ iash/cmd/EchoCommand.cpp
@@ -0,0 +1,19 @@
+#include "EchoCommand.h"
+using namespace std;
+
+
+virtual std::string getName () const {
+	return "echo";
+}
+
+virtual int run (UserCommand *cmd) {
+	ostream &os = cmd->getStdout();
+
+	const vector<string> &args = cmd->getWholeCommand();
+
+	for(auto it = ++(args.begin()); it != args.end(); ++it) {
+		os << *it;
+	}
+
+	return 0;
+ }
