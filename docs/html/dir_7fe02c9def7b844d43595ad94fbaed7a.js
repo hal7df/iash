a6952e0d48063e1a4a921ac2433a4ed73ab8ecb7
Paul Bunyan <hal7df@gmail.com>
2016-12-13 15:35:02 -0500

    Fix syntax error in CommandDispatcher documentation.

--- docs/html/dir_7fe02c9def7b844d43595ad94fbaed7a.js
+++ docs/html/dir_7fe02c9def7b844d43595ad94fbaed7a.js
@@ -4,7 +4,6 @@ var dir_7fe02c9def7b844d43595ad94fbaed7a =
     [ "Command.h", "_command_8h_source.html", null ],
     [ "CommandDispatcher.h", "_command_dispatcher_8h_source.html", null ],
     [ "CommandPreprocessor.h", "_command_preprocessor_8h_source.html", null ],
-    [ "crosslib.h", "crosslib_8h_source.html", null ],
     [ "Directory.h", "_directory_8h_source.html", null ],
     [ "Environment.h", "_environment_8h_source.html", null ],
     [ "iash.h", "iash_8h_source.html", null ],
